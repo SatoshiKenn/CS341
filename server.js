26fefc184c4b89eede5cbe889e4ecaf3fc2631ed
Kennette Guevara <92239496+SatoshiKenn@users.noreply.github.com>
2023-04-30 01:48:31 -0400

    Mongo DB connection

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config();
 const app = express();
 
 const port = 3000;
@@ -8,3 +10,22 @@ app.use('/', require('./routes'));
 app.listen(process.env.port || port , () => {
     console.log('Web Server is running at port ' + (process.env.port || 3000))
 });
+
+mongoose.connect(process.env.connection).then(() => 
+{
+          console.log("\n");
+          console.log("*******************************");
+          console.log("✔ Mongo Successfully Connected!");
+          console.log("*******************************");
+          console.log("\n");
+        },
+        err => {
+          console.log("\n");
+          console.log("*******************************");
+          console.log("    Mongo Connection Failed    ");
+          console.log("*******************************");
+          console.log("\n");
+          console.log(err);
+        }
+      
+);
