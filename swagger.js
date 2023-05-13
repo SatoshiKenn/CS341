cf00916742ccbbf8e095341bf5c63c6fdfaa4672
Kennette Guevara <92239496+SatoshiKenn@users.noreply.github.com>
2023-05-13 14:47:34 -0400

    swagger render url update

--- swagger.js
+++ swagger.js
@@ -2,7 +2,7 @@ const swaggerAutogen = require('swagger-autogen')();
 
 const doc = {
   info: { title: 'CSE 341 API', description: 'Kennette Guevara API for CSE341' },
-  host: 'localhost:8080',
+  host: 'https://cs341.onrender.com/',
   schemes: ['http'],
 };
 
